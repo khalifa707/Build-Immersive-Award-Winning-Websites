55cc9fe6d036fd02016eddbd0a3fc8c9f2279795
Khalifa <bodakhalifa707@gmail.com>
2026-06-16 10:28:47 +0300

    Custom Cursor animation with Lerp, Optimizing requestAnimation Frame and the Snap Effect on Hover

--- src/app/(experiments)/6-variables/starter/index.tsx
+++ src/app/(experiments)/6-variables/starter/index.tsx
@@ -2,8 +2,41 @@
 
 import { cn } from "@/lib/utils";
 import s from "./styles.module.css";
+import {useEffect} from "react";
+import {distance} from "@/lib/math";
 
 export default function Page() {
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const controller = new AbortController();
+
+    window.addEventListener(
+        "mousemove",
+        (event) => {
+          if (!titleRef.current) return;
+
+          const { innerWidth, innerHeight } = window;
+          const centerX = innerWidth / 2;
+          const centerY = innerHeight / 2;
+
+          const maxDist = distance(0, 0, centerX, centerY);
+          let dist = distance(centerX, centerY, event.clientX, event.clientY);
+
+          dist /= maxDist;
+
+          titleRef.current.style.setProperty("--distance", dist.toString());
+        },
+        {
+          signal: controller.signal,
+        }
+    );
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
   return (
     <div
       className={cn(
